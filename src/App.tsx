5b64b9826719eb94ff03850872382ef1a7e8ad72
yukicoder0509 <bendoris0509@gmail.com>
2025-03-07 22:29:39 +0800

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+} from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import { Layout } from "./components/Layout";
+
 export default function App() {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Homepage />} />
+            </Route>,
+        ),
     );
+    return <RouterProvider router={router} />;
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="w-full">
+            <h1>楊皓程的個人網頁</h1>
+        </div>
+    );
+}
