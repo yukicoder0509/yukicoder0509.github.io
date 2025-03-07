9e8ac2917d642fdba190638409c72ee692f78d89
yukicoder0509 <bendoris0509@gmail.com>
2025-03-07 22:41:36 +0800

    feat: add simple header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
     return (
-        <div className="w-full">
-            <h1>楊皓程的個人網頁</h1>
+        <div className="w-full bg-blue-400">
+            <h1 className="p-4 text-center font-sans text-4xl font-bold text-white">
+                楊皓程的個人網頁
+            </h1>
         </div>
     );
 }
