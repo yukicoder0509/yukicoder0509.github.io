b8335eb0ee72958772ca991559d260845f51576e
yukicoder0509 <bendoris0509@gmail.com>
2025-06-17 16:29:55 +0800

    format: rearrange the import in index .css by prettier

--- src/components/AppShell/Header.tsx
+++ src/components/AppShell/Header.tsx
@@ -1,12 +1,8 @@
 export default function Header() {
     return (
-        <div className="w-full h-12 justify-end flex gap-5 px-5 py-2.5 font-sans">
-            <div>
-                Projects
-            </div>
-            <div>
-                About
-            </div>
+        <div className="flex h-12 w-full justify-end gap-5 px-5 py-2.5 font-sans">
+            <div>Projects</div>
+            <div>About</div>
         </div>
     );
 }
