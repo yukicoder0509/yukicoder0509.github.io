9a80c3984e4bcc0b6dc692f7da500c91c4305b27
yukicoder0509 <bendoris0509@gmail.com>
2025-06-17 16:33:20 +0800

    feat: add page title in home page

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,15 +1,21 @@
+import { Helmet } from "react-helmet";
 import WelcomeTitle from "../components/Home/WelcomeTitle.tsx";
 import SelfIntroCard from "../components/Home/SelfIntroCard.tsx";
 
 export default function Homepage() {
     return (
-        <div className="mx-auto mt-2.5 flex h-full max-w-4xl flex-col items-center px-5">
-            <div className="my-9 w-full">
-                <WelcomeTitle />
+        <>
+            <Helmet>
+                <title>Home</title>
+            </Helmet>
+            <div className="mx-auto mt-2.5 flex h-full max-w-4xl flex-col items-center px-5">
+                <div className="my-9 w-full">
+                    <WelcomeTitle />
+                </div>
+                <div className="mx-6 my-9 w-full md:mx-0">
+                    <SelfIntroCard />
+                </div>
             </div>
-            <div className="mx-6 my-9 w-full md:mx-0">
-                <SelfIntroCard />
-            </div>
-        </div>
+        </>
     );
 }
