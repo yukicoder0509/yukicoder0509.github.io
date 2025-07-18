f33da629e07ced594cb08f1a0d337fd1edeaa8c3
yukicoder0509 <bendoris0509@gmail.com>
2025-07-18 11:59:54 +0800

    feat: add self intro of mine

--- src/components/Home/SelfIntroCard.tsx
+++ src/components/Home/SelfIntroCard.tsx
@@ -8,10 +8,17 @@ export default function SelfIntroCard() {
                 className="size-40 rounded-full md:size-50"
             />
             <div className="font-serif text-xl md:max-w-1/2">
-                Self intro Lorem ipsum dolor sit amet consectetur. Dui at in nec
-                diam eu. Turpis consectetur aliquet molestie condimentum orci
-                posuere nulla id massa. Neque leo aliquam lacus adipiscing id.
-                Sed lacinia tortor mattis facilisis eu viverra.
+                <p>
+                    Currently study Computer Science at National Yang Ming Chiao
+                    Tung University, Taiwan. I am a Frontend Engineer with a
+                    passion for education and open source. I love to create
+                    things that make people’s lives easier and more enjoyable.
+                </p>
+                <p>
+                    I serve in NYCU Software Development Club as a frontend
+                    developer, education, and project manager. We aim to build
+                    welcoming community for students to learn and grow together.
+                </p>
             </div>
         </div>
     );
